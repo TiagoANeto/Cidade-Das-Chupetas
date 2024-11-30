8dd8594c9d7a0a7b9e7d70f04f9528d4e77c6d2a
TiagoANeto <tiago98.ta@gmail.com>
2024-11-30 01:43:36 -0300

    mudando movimentacao para rigidbody

--- CDC-Monografia/Assets/Scripts/PlayerMovement.cs
+++ CDC-Monografia/Assets/Scripts/PlayerMovement.cs
@@ -3,32 +3,33 @@ using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Vector3 movement;
     public Vector2 movementInput;
     InputMap inputMap;
-    private CharacterController cc;
+    private Rigidbody2D rb;
     private Animator animator;
-    
+
     void Awake()
     {
         inputMap = new InputMap();
         inputMap.PlayerActions.Move.performed += Move;
         inputMap.PlayerActions.Move.canceled += Move;
-        cc = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Movement();
+        Gravity();
     }
-    public void Movement()
+    private void Movement()
     {
-        cc.Move(movement * speed * Time.deltaTime);
+        transform.Translate(movement * speed * Time.deltaTime);
         
         if(movement.magnitude > 0.1f)
         {
@@ -39,6 +40,12 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("isRunning", false);
         }
     }
+
+    private void Gravity()
+    {
+        
+    }
+
     public void Move(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
