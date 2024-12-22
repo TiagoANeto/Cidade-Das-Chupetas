31f07c819224de5bf6a8b58bd3088f7e03e2d69e
TiagoANeto <tiago98.ta@gmail.com>
2024-12-22 15:45:23 -0300

    comentando codigos e add sprite chupeta obstaculo

--- CDC-Monografia/Assets/Scripts/Player/PlayerData.cs
+++ CDC-Monografia/Assets/Scripts/Player/PlayerData.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Player Data")]
+//Player data é uma classe de variaveis e metodos auxiliares do player, que vai armazenar alguns valores, checagens e funcoes prontas que seram utilizadas no player 
+
+[CreateAssetMenu(menuName = "Player Data")] 
 public class PlayerData : ScriptableObject
-{
+{   
+    //variaveis de calculo auxiliar 
     [Header("Gravity")]
     public float gravityStrength;
     public float gravityScale;
@@ -12,12 +15,13 @@ public class PlayerData : ScriptableObject
 
     [Space(20)]
 
+    //variaveis de calculo auxilar do jump
     [Header("Jump")]
-
     public float jumpHeigth;
     public float jumpTime;
     public float jumpForce;
-
+    
+    //checagem e calculo padrao para definir a gravidade 
     private void OnChecks()
     {
         gravityStrength = -(2 * jumpHeigth) / (jumpTime * jumpTime);
